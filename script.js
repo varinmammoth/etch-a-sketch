1bbb6e199abce7758938d74048fe83973c272514
varinmammoth <varinmammoth@gmail.com>
2021-08-26 14:33:26 +0700

    box.setAttribute is not a function error.

--- script.js
+++ script.js
@@ -4,20 +4,32 @@ let numCol = 8;
 let numRow = 8;
 let numBox = numCol * numRow;
 
+//Add grid-template-columns and grid-template-rows style to the gridContainer.
 function setGridColRow(numCol, numRow) {
     let setStyleStr = 'grid-template-columns: repeat(' + numCol.toString() + ', 1fr); grid-template-rows: repeat(' + numRow.toString() + ', 1fr)';
     gridContainer.setAttribute('style', setStyleStr);
 }
 
+//Add boxes to the gridContainer.
 function addGridBoxes(numBox) {
     for (let i=0; i<numBox; i+=1) {
+        //Adds the grid to the gridContainer.
         let gridBox = document.createElement('div');
         gridBox.setAttribute('class', 'gridBox');
         gridBox.textContent = i
         gridContainer.appendChild(gridBox);
     }
+
+    //Adds functionality to the boxes.
+    gridBox = document.querySelectorAll('.gridBox');
+    gridBox.forEach(box => {
+        box.addEventListener('mouseover', (box) => mouseOverEvent(box));
+        box.addEventListener('mouseout', (box) => mousOutEvent(box));
+        box.addEventListener('mousedown', (box) => mouseDownEvent(box));
+    });
 }
 
+//Sets up the Grid.
 function setUpGrid(numCol, numRow) {
     let numBox = numCol * numRow;
     setGridColRow(numCol, numRow);
@@ -26,5 +38,18 @@ function setUpGrid(numCol, numRow) {
 
 setUpGrid(numCol, numRow);
 
+//Box functionality functions.
+function mouseOverEvent(box) {
+    box.setAttribute('style', 'background-color: grey;');
+}
 
+function mouseOutEvent(box) {
+    setTimeout((box) => {
+        box.setAttribute('style', 'background-color: transparent;');
+    }, 500);
+}
+
+function mouseDownEvent(box) {
+    box.setAttribute('style', 'background-color: black;');
+}
 
